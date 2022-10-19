272bd304c76fd576884037ef97f86033f09df870
Simon Fremaux <dievardump@gmail.com>
2022-10-19 15:52:00 +0200

    footer menu mobile

--- src/lib/components/sections/Footer.svelte
+++ src/lib/components/sections/Footer.svelte
@@ -1,9 +1,17 @@
 <script>
 	import ButtonAbout from '../Buttons/ButtonAbout.svelte';
 	import ButtonTeam from '../Buttons/ButtonTeam.svelte';
+
+	let clientWidth = 0;
+	let width;
+	let half;
+	$: if (clientWidth) {
+		width = clientWidth % 2 == 0 ? clientWidth - 2 : clientWidth - 3;
+		half = clientWidth / 2;
+	}
 </script>
 
-<footer>
+<footer bind:clientWidth>
 	<nav class="buttons">
 		<div class="button">
 			<ButtonAbout />
@@ -16,21 +24,41 @@
 	<a href="/" class="logo">
 		<img src="/images/logo.png" alt="Lost Paradigms" />
 	</a>
+
+	{#if half}
+		<svg id="mask" class="mobile-bg" viewBox="0 0 {width} 86" {width} height="86" fill="none">
+			<path
+				d="M0 15H{half - 50}L{half - 35} 0H{half + 35}L{half + 50} 15H{width}V86H0z"
+				stroke="#39eeff"
+				stroke-width="2"
+			/>
+		</svg>
+	{/if}
 </footer>
 
 <style lang="postcss">
 	footer {
 		width: 100%;
-		max-width: 390px;
 		height: 86px;
 		position: absolute;
 		bottom: 0;
 
 		left: 50%;
 		transform: translateX(-50%);
-		background: url(/images/interface/footer-mobile.png) center center no-repeat;
-		background-size: 100% auto;
+		/* background: url(/images/interface/footer-mobile.png) center center no-repeat; */
+		/* background-size: 100% auto; */
 		overflow: hidden;
+		background: var(--box-back);
+	}
+
+	.mobile-bg {
+		@apply block sm:hidden;
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: -1;
 	}
 
 	.buttons {
@@ -58,7 +86,6 @@
 		footer {
 			height: 143px;
 			width: 980px;
-			max-width: none;
 			background: transparent;
 		}
 
